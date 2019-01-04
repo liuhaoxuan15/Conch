d673c78f2668f9bfa7674f6af8ea530a35004b0c
liuhaoxuan <703929634@qq.com>
2019-01-04 21:57:17 +0800

    think

--- application/class_admin/controller/Register.php
+++ application/class_admin/controller/Register.php
@@ -6,9 +6,26 @@ class Register extends Controller
 {
     public function index()
     {
-    	// $clubs = Db::query('select * from club_club');
-    	// $this->assign("clubs",$clubs);
         return $this->fetch();
 	}
+    public function register() {
+        if (request()->isPost()) {
+            $data = [
+                'class_name'=>input('param.class_name'),
+                'class_peopleNumber'=>input('param.class_peopleNumber'),
+                'class_time'=>input('param.class_time'),
+                'class_info'=>input('param.class_info'),
+                'class_position'=>input('param.class_position'),
+            ];
+        }
 
+        //存入数据库
+        $res = Db::name("classes")->insert($data);
+        if($res){
+            return alert_success("申请成功，请耐心等待审核通过。",'index/index');
+        } else {
+            return alert_error("申请失败");
+        }
+
+    }
 }
