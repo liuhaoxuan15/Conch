d7703cd0fe8625d17f46fa65989bc25a9419c784
liuhaoxuan15 <703929634@qq.com>
2019-02-27 14:55:10 +0800

    教师列表

--- application/class_admin/controller/Teacher.php
+++ application/class_admin/controller/Teacher.php
@@ -13,4 +13,17 @@ class Teacher extends Controller
         return $this->fetch();
 	}
 
+    public function getTeachersList() {
+        // $res = Db::query('select * from classes');
+        $class = \think\Session::get('class_admin');
+        $class_id = $class[class_id];
+        // echo($class_id);
+        $res = Db::query("select * from users u, teaching t where t.class_id = '$class_id' and t.user_id = u.user_id");
+        // $res = Db::name('users')->where('user_type',1)->select();
+        $count = Db::name('teaching')->where('class_id',$class_id)->count();
+        $this->assign("TList",$res); 
+        // $listres = $list->append('[$res]')->toJson();
+        // dump($res);
+        return json(['code'=>0,'count'=>$count,'data'=>$res]);
+    }
 }
